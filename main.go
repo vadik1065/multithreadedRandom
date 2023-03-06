bc11ceb0715a18d019c78955b85c162ecfa9edb1
aalplotnikov <aalplotnikov@gmail.com>
2023-03-06 11:53:19 +0400

    added a stop when there are all numbers

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 
 var wg sync.WaitGroup
 
-// вывод среза
+// printSlice - вывод среза
 func printSlice(s *[]int) {
 	fmt.Printf(" %v\n", *s)
 }
 
-// включает ли срез элимент
+// contains - включает ли срез элимент
 func contains(numbers []int, number int) bool {
 	for _, v := range numbers {
 		if v == number {
@@ -25,39 +25,60 @@ func contains(numbers []int, number int) bool {
 	return false
 }
 
-// генирим рандомное число
-func addToArrayRandomNumber(randomNumbers *[]int, minNumber *int, maxNumber *int) {
-	fmt.Println("init")
-
+// addToArrayRandomNumber - генирим рандомное число
+func addToArrayRandomNumber(randomNumbers *[]int, minNumber int, maxNumber int, endGeneration *int) {
+	number := rand.Intn(maxNumber-minNumber) + minNumber
 	// time.Sleep(1 * time.Second)
-	number := rand.Intn(*maxNumber-*minNumber) + *minNumber
-	fmt.Println(number)
+	time.Sleep(time.Duration(number) * time.Second)
+	fmt.Printf("сгенерировано число %d \n", number)
+
 	if !contains(*randomNumbers, number) {
 		*randomNumbers = append(*randomNumbers, number)
 	}
+	*endGeneration++
+}
+
+// awaitGetAllNumber - дожидается получения всех чисел
+func awaitGetAllNumber(countNumber int, countBlock int) {
+	minNumber := 0
+	maxNumber := countNumber
+
+	var randomNumbers []int
+	countAvailableGorutine := countBlock
+	// endGeneration := 0
+
+	// выполняется пока нет нужного количества чисел
+	for len(randomNumbers) != countNumber {
+		rand.Seed(time.Now().UnixNano())
+		if countAvailableGorutine > 0 {
+			countAvailableGorutine--
+			// потоки с раномными числами
+			go addToArrayRandomNumber(&randomNumbers, minNumber, maxNumber, &countAvailableGorutine)
+		}
+		// else if endGeneration != 0 {
+		// 	countAvailableGorutine += endGeneration
+		// 	endGeneration = 0
+		// }
+
+	}
+
 	defer wg.Done()
+	printSlice(&randomNumbers)
+
 }
 
 func main() {
-	var randomNumbers []int
 
 	// парсим флаги
-
-	var countBlock = flag.Int("count", 3, "sets the count block for random func")
-	var minNumber = flag.Int("min", 1, "sets the min number for random func")
-	var maxNumber = flag.Int("max", 10, "sets the max number for random func")
+	var countBlock = flag.Int("blocks", 3, "sets the count block for random func")
+	var countNumber = flag.Int("numbers", 10, "sets the count block for random func")
+	// var minNumber = flag.Int("min", 1, "sets the min number for random func")
+	// var maxNumber = flag.Int("max", 10, "sets the max number for random func")
 	flag.Parse()
 
-	// fmt.Println(*countBlock)
+	wg.Add(1)
+	go awaitGetAllNumber(*countNumber, *countBlock)
 
-	// потоки с раномными числами
-	rand.Seed(time.Now().UnixNano())
-	for i := 1; i <= *countBlock; i++ {
-		wg.Add(1)
-		go addToArrayRandomNumber(&randomNumbers, minNumber, maxNumber)
-	}
 	wg.Wait()
-
-	printSlice(&randomNumbers)
 
 }
